fc646237df05d15cad0a692d7b18c241cd65b0c4
DEV <DEV@DESKTOP-8VP4IIT>
2024-05-15 11:07:14 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import CoursesScreen from './src/screens/CoursesScreen';
+import BoxScreen from './src/screens/BoxScreen';
+import CoursesInformationScreen from './src/screens/CoursesInformationScreen';
+import CounterScreen from './src/screens/CounterScreen';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Anasayfa" component={HomeScreen} />
+        <Stack.Screen name="Kurslarım" component={CoursesScreen} />
+
+        <Stack.Screen name="KursBilgilerim" component={CoursesInformationScreen} />
+        <Stack.Screen name="Sayac" component={CounterScreen} />
+        <Stack.Screen name="Kutu" component={BoxScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
